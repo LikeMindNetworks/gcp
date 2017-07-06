98202756a83c79fea9de40bfb3ff56e26249a734
Ying Wei Loke <kukikiloke@gmail.com>
2017-07-06 17:44:26 -0400

    travis with multi projects

---
+++ gc-pubsub/src/pubsub-client.spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const pubsubClient = require('./pubsub-client');
+
+describe('Pubsub-client', function() {
+	it('Main Handler', () => {
+
+		// index.imageProcessing();
+	});
+});
---
+++ gc-vision/index.spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const index = require('./index');
+
+describe('Vision Cloud Function Handler', function() {
+	it('Main Handler', () => {
+
+		// index.imageProcessing();
+	});
+});
